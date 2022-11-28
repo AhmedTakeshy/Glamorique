ec0eeeac8e34705c6a3c8bd2fa58f1f2c670791e
Ahmed Abdelsamie <Ahmedtakeshy7@gmail.com>
2022-11-28 03:39:27 +0300

    Modifying partials files and adding to top button

--- public/js/main.js
+++ public/js/main.js
@@ -5,43 +5,70 @@ let menu_Icon = document.querySelector(".menu-icon");
 let side_Menu = document.querySelector(".side_menu");
 
 // start open side menu form BTn
-menu_Icon.addEventListener("click",() => {
-    side_Menu.classList.add("active");
-})
+menu_Icon.addEventListener("click", () => {
+  side_Menu.classList.add("active");
+});
 // end open side menu form BTn
-///////////////////////////////////////////////////////////////////////////
+
 // start close Side menu form anywere
-document.body.addEventListener("click",(e) => {
-    if(e.target.classList.contains("side_menu") || e.target.classList.contains("menu-icon") || e.target.classList.contains("Link_Section")
-        || e.target.classList.contains("Header_linke") || e.target.classList.contains("image_section") || e.target.classList.contains("img_slider")) {
-    } else {
-        side_Menu.classList.remove("active");
-    }
-})
+document.body.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("side_menu") ||
+    e.target.classList.contains("menu-icon") ||
+    e.target.classList.contains("Link_Section") ||
+    e.target.classList.contains("Header_linke") ||
+    e.target.classList.contains("image_section") ||
+    e.target.classList.contains("img_slider")
+  ) {
+  } else {
+    side_Menu.classList.remove("active");
+  }
+});
 // end close Side menu form anywere
 //  start chang content of side menu
-AddAcvtive()
+AddAcvtive();
 function AddAcvtive() {
-    list_menu_links.forEach(section => {
-        section.addEventListener("click",(e) => {
-            document.querySelectorAll(".list_menu li").forEach(li => {
-                li.classList.remove("active");
-            })
-            e.target.classList.add("active")
-            image_section.forEach(imgSection => {
-                imgSection.classList.remove("active")
-                if(e.target.dataset.type == imgSection.dataset.img) {
-                    imgSection.classList.add("active");
-                }
-            })
-            sections_linkes.forEach(links => {
-                links.classList.remove("active");
-                if(e.target.dataset.type == links.dataset.name ) {
-                    links.classList.add("active");
-                }
-            })
-        })
-    })
+  list_menu_links.forEach((section) => {
+    section.addEventListener("click", (e) => {
+      document.querySelectorAll(".list_menu li").forEach((li) => {
+        li.classList.remove("active");
+      });
+      e.target.classList.add("active");
+      image_section.forEach((imgSection) => {
+        imgSection.classList.remove("active");
+        if (e.target.dataset.type == imgSection.dataset.img) {
+          imgSection.classList.add("active");
+        }
+      });
+      sections_linkes.forEach((links) => {
+        links.classList.remove("active");
+        if (e.target.dataset.type == links.dataset.name) {
+          links.classList.add("active");
+        }
+      });
+    });
+  });
 }
 //  end chang content of side menu
 
+$(document).ready(function () {
+  const btnToTop = $(".backTop");
+  $(window).on("scroll", scrollTopFade);
+  $(btnToTop).on("click", function () {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      500
+    );
+    return false;
+  });
+
+  function scrollTopFade() {
+    if ($(window).scrollTop() > 100) {
+      $(btnToTop).fadeIn();
+    } else {
+      $(btnToTop).fadeOut();
+    }
+  }
+});
